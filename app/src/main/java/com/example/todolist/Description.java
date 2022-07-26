87fd51019ed1a2fb31754634d3fa36f4b6a3f10b
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2022-07-26 17:34:49 +0300

    ДЗ 6 пп. 1-6.

--- app/src/main/java/com/example/todolist/Description.java
+++ app/src/main/java/com/example/todolist/Description.java
@@ -3,13 +3,9 @@ package com.example.todolist;
 import java.util.Calendar;
 
 public class Description {
-<<<<<<< HEAD
+
     private String name;
     private String description;
-=======
-    private final String name;
-    private final String description;
->>>>>>> f65ca63 (ДЗ 6 пп. 1-6.)
     private Calendar date;
 
     public Description(String name, String description, Calendar date) {
